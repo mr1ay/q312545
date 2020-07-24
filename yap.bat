da522e788d472dd696e2951b9ddc0d63ab6bbe16
mr1ay <48817265+mr1ay@users.noreply.github.com>
2020-07-24 06:16:44 +0300

    Add files via upload

---
+++ yap.bat
@@ -0,0 +1,12 @@
+ timeout /t 5
+@echo off
+::del geometri.bat
+::del hf.bat
+
+set aa1=C:\Users\ZEKİYE\Desktop\geo
+move kaynak2\*.* %aa1%
+
+set /p a=<ver.txt
+echo.%a% >v1.mr1ay 
+
+::rd /s /q kaynak
