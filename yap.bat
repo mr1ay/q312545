60de266b3e01165c86e89a182b191288f4d7b223
mr1ay <48817265+mr1ay@users.noreply.github.com>
2020-07-24 06:46:49 +0300

    Update yap.bat

--- yap.bat
+++ yap.bat
@@ -4,8 +4,8 @@ call consolelocation
 ::del geometri.bat
 ::del hf.bat
 
-set aa1=C:\Users\ZEKİYE\Desktop\geo
-move kaynak2\*.* %aa1%
+set aa1=%cd%
+move kaynak\*.* %aa1%
 
 set /p a=<ver.txt
 echo.%a% >v1.mr1ay 
