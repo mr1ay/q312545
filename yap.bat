dd6eabf131dc1fee8885ba8a428127c236b251cc
mr1ay <48817265+mr1ay@users.noreply.github.com>
2020-07-27 23:36:09 +0300

    Update yap.bat

--- yap.bat
+++ yap.bat
@@ -9,12 +9,13 @@ set /p a=<ver.txt
 echo.%a%>v1.mr1ay 
 
 
-rd /s /q kaynak
 
 move kaynak\fx_by_mr1ay %cd%
 move kaynak\a.bat %cd%
 move kaynak\insertbmp.exe %cd%
 cd..
 move core2\kaynak\core1 %cd%
+cd core2
 
+rd /s /q kaynak
 exit
