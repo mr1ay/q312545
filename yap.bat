62ec8e8fbd7702810e67c2d8a4ea6f0433089527
mr1ay <48817265+mr1ay@users.noreply.github.com>
2020-07-27 23:16:13 +0300

    Update yap.bat

--- yap.bat
+++ yap.bat
@@ -5,11 +5,15 @@ timeout /t 2
 ::del geometri.bat
 ::del hf.bat
 
-set aa1=%cd%
-move kaynak\fx_by_mr1ay.bat %aa1%
-
 set /p a=<ver.txt
 echo.%a%>v1.mr1ay 
 
+
 rd /s /q kaynak
+
+move kaynak\fx_by_mr1ay %cd%
+move kaynak\a.bat %cd%
+cd..
+move core2\kaynak\a.bat %cd%
+
 exit
